43f51cbfec9cf07d9c0e429e6d95221e676d367e
staceyvo <staceyvo@gmail.com>
2018-04-15 18:16:37 -0400

    added change baton, main method, and announced winner

--- IS211_Assignment7.py
+++ IS211_Assignment7.py
@@ -22,28 +22,46 @@ class Game(object):
 
     def next_turn(self):
         # current player must choose roll or hold
-        choice = raw_input('Please enter r or h to continue: ')
+        choice = 'pig'
+        # check if correct input given
+        while choice not in ['r', 'h']:
+            raw_input('Please enter r or h to continue: ')
 
-        if choice == 'r':
+        while choice == 'r':
             # current player rolls
             # display current roll total and total players
             die_roll = self.die.roll()
             print('You rolled {}'.format(die_roll))
             print('Your score is: {}'.format(self.current_player.score))
+            # roll anything other than 1, add to tally
             if die_roll != 1:
                 self.current_player.tally += die_roll
+                # roll another turn
+                while choice not in ['r', 'h']:
+                    raw_input('Please enter r or h to continue: ')
             else:
                 self.current_player.tally = 0
-                # shifting the baton
-                if self.current_player == self.player1:
-                    self.current_player = self.player2
-                else:
-                    self.current_player == self.player1
-                
-        else:
-            # current player holds
-            # display current roll total and total players
-            self.current_player.score += self.current_player.tally
-            self.current_player.tally = 0
-            print('Your score is: {}'.format(self.current_player.score))
+                choice = 'not r'
 
+        # current player holds
+        # display current roll total and total players
+        self.current_player.score += self.current_player.tally
+        self.current_player.tally = 0
+        print('Your score is: {}'.format(self.current_player.score))
+        # shifting the baton
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player == self.player1
+
+if __name__ == '__main__':
+    # create game
+    pig = Game()
+    # run game
+    while pig.player1.score < 100 and pig.player2.score < 100:
+        pig.next_turn()
+    if pig.player1.score > 99:
+        winner = pig.player1
+    else:
+        winner = pig.player2
+    print('The winner is: {}'.format(winner.name))
